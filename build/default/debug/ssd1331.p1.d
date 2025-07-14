d9e3ccea9be45ef41c67cb5f65dd8e18f838345e
Bruno Tondin <btondin@gmail.com>
2025-07-14 17:33:48 -0300

    Delete build/default directory

--- build/default/debug/ssd1331.p1.d
+++
@@ -1,8 +0,0 @@
-build/default/debug/ssd1331.p1:  \
-ssd1331.c  \
-ssd1331.h  \
-mcc_generated_files/mcc.h  \
-mcc_generated_files/device_config.h  \
-mcc_generated_files/pin_manager.h  \
-mcc_generated_files/spi1.h  \
-gfx_pic.h 
